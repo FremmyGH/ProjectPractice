c050ad90af3a7cd11296f3e5c750891bd9712ebd
FremmyGH <38381533+FremmyGH@users.noreply.github.com>
2019-04-01 20:12:45 +0500

    fixes

--- lab_1/day.php
+++ lab_1/day.php
@@ -6,7 +6,9 @@ $date = $_GET['date'];
 $str = explode(".", $date);
 $result = date("l", mktime(0,0,0, $str[1], $str[0], $str[2]));
 
-echo json_encode($result);
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
 
 require_once "saveResult.php";
 Save("../lab_1.json", $result);
+
+
--- lab_1/region.php
+++ lab_1/region.php
@@ -2,11 +2,10 @@
 header('Content-Type: application/json');
 
 $n = basename($_SERVER['REQUEST_URI']);
-$codes = json_decode(file_get_contents("region.json"));
+$codes = json_decode(file_get_contents("regions.json"));
 
 $result = $codes->$n;
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
 
 require_once "saveResult.php";
 Save("../lab_1.json", $result);
-//phpinfo();
